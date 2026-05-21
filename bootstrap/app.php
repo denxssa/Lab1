f091e08ac83662f186ad813a4842abf0eb3f901d
Denxssa <deniixsa18@gmail.com>
2026-05-21 02:09:06 +0200

    added dynamic team members management for About Us

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/team-members',
+            'api/team-members/*',
+            'api/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- resources/js/views/Admin-View/AdminViewLayout.js
+++ resources/js/views/Admin-View/AdminViewLayout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import './components/shared/AdminShared.scss';
 import TopBar from './components/shared/TopBar/TopBar';
+import AdminSideBar from './components/shared/Sidebar/AdminSidebar';
 
 const AdminViewLayout = () => {
   return (
     <div className="admin-layout">
+      <AdminSideBar />
       <div className="admin-layout-content">
         <TopBar />
         <Outlet />
