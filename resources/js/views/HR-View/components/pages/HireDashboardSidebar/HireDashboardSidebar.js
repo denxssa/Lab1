57a6921296bd497ae7524a07d56fc12e7f8850fa
Denxssa <deniixsa18@gmail.com>
2026-05-21 16:42:53 +0200

    added Home button to sidebar

--- resources/js/views/HR-View/components/pages/HireDashboardSidebar/HireDashboardSidebar.js
+++ resources/js/views/HR-View/components/pages/HireDashboardSidebar/HireDashboardSidebar.js
@@ -113,6 +113,15 @@ const HireDashboardSidebar = () => {
                 <span className="hire-nav-label">{item.label}</span>
               </button>
             ))}
+            <button
+              className="hire-nav-item hire-nav-home"
+              onClick={() => handleNav('/')}
+            >
+              <span className="hire-nav-icon">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
+              </span>
+              <span className="hire-nav-label">Home</span>
+            </button>
           </nav>
         </div>
 
